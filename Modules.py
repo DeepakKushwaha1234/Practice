9c785224087a892e8260be367ab487f8dffeba4c
deepak <dkushwaha8083@gmail.com>
2024-12-13 22:40:57 +0530

    firstcommit

---
+++ Modules.py
@@ -0,0 +1,12 @@
+# modules are already written program that we imports and use it and its save our time to write that large program
+
+ex. Flask , pyjoke, django etc
+
+how to istall module in vs code 
+- just open terminal and type
+  pip install flask
+  pip install pyjoke
+
+
+
+2types of module 
